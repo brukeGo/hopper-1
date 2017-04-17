f10c57034560e14f557df59810f99d3c2a00da23
Tera Byte <terabytes@users.noreply.github.com>
2017-04-16 20:44:30 -0700

    - Modify Express for MVC, split controllers+routes

---
+++ app_server/routes/index.js
@@ -0,0 +1,17 @@
+/* 
+  ROUTERS
+    Purpose:  Routing maps URL requests to controllers
+    Example:  router.get('/') is where the router looks
+                for a get request on the homepage URL path ('/')
+*/
+
+var express = require('express');
+var router = express.Router();
+
+// Require controller file(s)
+var ctrlMain = require('../controllers/main');
+
+/* GET home page. */
+router.get('/', ctrlMain.index); // Reference index method of ctrlMain controller in this route definition
+
+module.exports = router;
