5d934ac630a59f52220157aa238f491ee43cc3e2
Alejandra Gonzalez <gonzalez.alejandra.013@gmail.com>
2017-04-18 14:54:23 -0700

    More Routes
    Setting up routes for future pages.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -14,4 +14,17 @@ var ctrlMain = require('../controllers/main');
 /* GET home page. */
 router.get('/', ctrlMain.index); // Reference index method of ctrlMain controller in this route definition
 
+/*Comment by Ale:
+Once you get the thing running on your local host, you'll
+be able to load different URLs by typing stuff like "localhost:3000/createEvent" in the URL bar for the future
+create events page. At the time of writing, you'll see the login page but with different URLs.
+*/
+router.get('/createEvent', ctrlMain.createEvent);
+router.get('/postEvent', ctrlMain.postEvent);
+router.get('/saveEvent', ctrlMain.saveEvent);
+router.get('/recover', ctrlMain.recover);
+router.get('/viewEvent', ctrlMain.viewEvent);
+router.get('/welcome', ctrlMain.welcome);
+router.get('/signUp', ctrlMain.signUp);
+
 module.exports = router;
