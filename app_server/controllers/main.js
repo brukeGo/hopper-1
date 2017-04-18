14fa4e03f10f69c1df4ef21b267a90b3779a5918
Alejandra Gonzalez <gonzalez.alejandra.013@gmail.com>
2017-04-18 14:54:45 -0700

    More Controllers
    Controllers for future pages.

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -19,3 +19,31 @@ module.exports.index = function(req, res) {
   */
   res.render('index', { title: 'Hopper' });
 }
+
+/* Comment by Ale:
+The index.jade file at the time of writing currently displays the
+login page so all the pages show the login page. Once the other jade files are finished,
+please change 'index' to the correct jade file.
+@Tera: Idk why the titles don't work yet. Also, we'll organize these controllers later.
+*/
+module.exports.createEvent = function(req, res) {
+res.render('index', {title: 'Create Event'});
+}
+module.exports.postEvent = function(req, res) {
+res.render('index', {title: 'Post Event'});
+}
+module.exports.saveEvent = function(req, res) {
+res.render('index', {title: 'Save Event'});
+}
+module.exports.recover = function(req, res) {
+res.render('index', {title: 'Recover Password'});
+}
+module.exports.viewEvent = function(req, res) {
+res.render('index', {title: 'Event'});
+}
+module.exports.welcome = function(req, res) {
+res.render('index', {title: 'Welcome'});
+}
+module.exports.signUp = function(req, res) {
+res.render('index', {title: 'Register'});
+}
