54286c513e4cb606db6f8d91bd9081038f2efaad
Brooke Gosselin <bgoss086@verizon.net>
2017-04-19 15:37:43 -0700

    Create event view
    routes updated

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -14,4 +14,5 @@ var ctrlMain = require('../controllers/main');
 /* GET home page. */
 router.get('/', ctrlMain.index); // Reference index method of ctrlMain controller in this route definition
 router.get('/welcome', ctrlMain.welcome);
+router.get('/createevent', ctrlMain.createevent);
 module.exports = router;
