039892fd3276e991e42a69391d75229c9a060774
terabytes <terabitbyte@gmail.com>
2017-04-28 16:24:27 -0700

    Fix url paths and views

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -16,6 +16,7 @@ router.get('/', ctrlMain.login); // Reference index method of ctrlMain controlle
 router.get('/welcome', ctrlMain.mainMenu);
 router.get('/event', ctrlMain.event);
 router.get('/event/new', ctrlMain.createEvent);
+router.get('/event/edit', ctrlMain.editEvent);
 router.get('/event/draft', ctrlMain.eventDraft);
 router.get('/event/saved', ctrlMain.eventSaved);
 router.get('/event/posted', ctrlMain.eventPosted);
@@ -23,9 +24,8 @@ router.get('/events', ctrlMain.events);
 router.get('/events/finder', ctrlMain.eventFinder);
 router.get('/events/search', ctrlMain.searchEvents);
 router.get('/events/filter', ctrlMain.filterEvents);
+router.get('/events/liked', ctrlMain.likedEvents);
 router.get('/events/my', ctrlMain.myEvents);
-router.get('/events/my/edit', ctrlMain.myEventsEdit);
-router.get('/events/my/liked', ctrlMain.myLikedEvents);
 router.get('/recover', ctrlMain.recover);
 router.get('/register', ctrlMain.register);
 router.get('/account', ctrlMain.account); // need to add view for this (user-menu)
