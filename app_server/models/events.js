46763b3708346d601b8c8843c8fa3007f734bb68
Alejandra Gonzalez <gonzalez.alejandra.013@gmail.com>
2017-04-28 20:21:23 -0700

    Event Schema

--- app_server/models/events.js
+++ app_server/models/events.js
@@ -1,2 +1,15 @@
 var mongoose = require( 'mongoose' );
-var eventSchema = new mongoose.Schema({ });
+
+var eventSchema = new mongoose.Schema({
+  title: {type: String, required: true},
+  img: {data: Buffer, contentType}, 
+  start: {type: Date, required: true},
+  end: Date,
+  location: {type: String, required: true},
+  description: String,
+  tags: [String],
+  posted: {type: boolean, default: false},
+  repeat: {repeat_every: Number, basis: String, occurences: Number}
+});
+
+mongoose.model('Event', eventSchema);
