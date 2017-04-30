3780d2a8866821af03702619d522a88ad78075cd
Tera Bytes <terabitbyte@gmail.com>
2017-04-30 14:58:30 -0700

    Create and connect to prod db

--- app_server/models/events.js
+++ app_server/models/events.js
@@ -1,2 +1,12 @@
 var mongoose = require( 'mongoose' );
-var eventSchema = new mongoose.Schema({ });
+var eventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  img: { data: Buffer, contentType: String }, 
+  start: { type: Date, required: true, "default": Date.now },
+  end: Date,
+  location: { type: String, required: true },
+  description: String,
+  tags: [ String ],
+  posted: { type: Boolean, "default": false },
+  repeat: { repeat_every: Number, basis: String, occurences: Number}
+});
