920d4bf523b602f28d517b522527431f5e176d80
Alejandra Gonzalez <gonzalez.alejandra.013@gmail.com>
2017-05-05 00:38:08 -0700

    Filtered Events List Controller

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -11,4 +11,7 @@ router.get('/events/:eventid', ctrlMain.readEvent);
 router.put('/events/:eventid', ctrlMain.updateEvent);
 router.delete('/events/:eventid', ctrlMain.deleteEvent);
 
+router.get('/events/tag', ctrlMain.taggedEventsList);
+router.post('/events/filter', ctrlMain.filteredEventsList);
+
 module.exports = router;
