ba7b9edfe67b5e7109760393156ae9d98e2e49a2
Alejandra Gonzalez <gonzalez.alejandra.013@gmail.com>
2017-05-10 21:40:21 -0700

    Edited comments

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -11,27 +11,27 @@ var router = express.Router();
 /* Require controller file(s) */
 var ctrlMain = require('../controllers/main');
 
-router.get('/', ctrlMain.login); 			              // log in page
-router.get('/welcome', ctrlMain.mainMenu);		      // welcome/event menu
-router.get('/event/new', ctrlMain.createEvent);		  // create an event
-router.get('/event/edit', ctrlMain.editEvent);		  // edit an event
-router.get('/event/draft', ctrlMain.eventDraft);	  // view an event draft
-router.post('/event/saved', ctrlMain.eventSaved);	  // event saved successfully! page
-router.get('/event/posted', ctrlMain.eventPosted);	// event posted successfully! page
-router.get('/event/:eventid', ctrlMain.event); 			// view a event
-router.get('/events', ctrlMain.events);			        // view a list of events
-router.get('/events/finder', ctrlMain.eventFinder);	// find event menu
-router.get('/events/search', ctrlMain.searchEvents);// search for events by keywords/tags
-router.post('/events/searched', ctrlMain.eventsSearched);
-router.get('/events/filter', ctrlMain.filterEvents);// checkboxes for filtering through events
-router.get('/events/liked', ctrlMain.likedEvents);	// show list of user's liked events
-router.get('/events/my', ctrlMain.myEvents);		    // show list of user's event posts/drafts
-router.get('/recover', ctrlMain.recover);		        // recover passwords
-router.get('/register', ctrlMain.register);		      // register for account
-router.get('/account', ctrlMain.account); 		      // user menu
-router.get('/account/edit', ctrlMain.editAccount);  // edit account info
-router.get('/verify', ctrlMain.verify);             // verify e-mail
+router.get('/', ctrlMain.login); 			                    // log in page
+router.get('/welcome', ctrlMain.mainMenu);		            // welcome/event menu
+router.get('/event/new', ctrlMain.createEvent);		        // create an event
+router.get('/event/edit', ctrlMain.editEvent);		        // edit an event
+router.get('/event/draft', ctrlMain.eventDraft);	        // view an event draft
+router.post('/event/saved', ctrlMain.eventSaved);	        // event saved successfully! page
+router.get('/event/posted', ctrlMain.eventPosted);	      // event posted successfully! page
+router.get('/event/:eventid', ctrlMain.event); 			      // view a event
+router.get('/events', ctrlMain.events);			              // view a list of events
+router.get('/events/finder', ctrlMain.eventFinder);	      // find event menu
+router.get('/events/search', ctrlMain.searchEvents);      // search for events by keywords/tags
+router.post('/events/searched', ctrlMain.eventsSearched); // view a list of events searched by keyword/tag
+router.get('/events/filter', ctrlMain.filterEvents);      // checkboxes for filtering through events
+router.get('/events/liked', ctrlMain.likedEvents);	      // show list of user's liked events
+router.get('/events/my', ctrlMain.myEvents);		          // show list of user's event posts/drafts
+router.get('/recover', ctrlMain.recover);		              // recover passwords
+router.get('/register', ctrlMain.register);		            // register for account
+router.get('/account', ctrlMain.account); 		            // user menu
+router.get('/account/edit', ctrlMain.editAccount);        // edit account info
+router.get('/verify', ctrlMain.verify);                   // verify e-mail
 router.post('/events/filtered', ctrlMain.filteredEvents); //view a list of filtered events
-router.get('/features', ctrlMain.features);         // List of features
+router.get('/features', ctrlMain.features);               // List of features
 
 module.exports = router;
